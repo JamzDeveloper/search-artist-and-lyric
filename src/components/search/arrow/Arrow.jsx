6190cfd1013fa0a5ff23affdb3269418c9ec620e
Jose Montenegro <jamz201616@gmail.com>
2021-05-29 13:16:06 -0500

    v 1.0

--- src/components/search/arrow/Arrow.jsx
+++ src/components/search/arrow/Arrow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./arrow.scss";
 const Arrow = () => {
   return (
-    <div className="container-arrow">
+    <div className="container--arrow">
       <div className="arrows"></div>
     </div>
   );
